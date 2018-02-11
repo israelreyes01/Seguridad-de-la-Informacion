482f5f6790086061a643193c513a49191ea3327b
Israel E. Reyes <IsraelR@Ccn147.cenosa.hn>
2018-02-11 01:01:23 -0600

    Agregue el archivo Modulo1

---
+++ Modulo1.py
@@ -0,0 +1,3 @@
+def Suma(x, y):
+	return x + y
+
---
+++ Script1.py
@@ -0,0 +1,9 @@
+
+#Mi Primer Script
+#Certificado de seguridad de la info.
+
+print("Programa que suma 2 numeros")
+a = int(input("Ingrese el sumando 1: "))
+b = int(input("Ingrese el sumando 2: "))
+suma = a + b
+print("El resultado es : ", suma)
